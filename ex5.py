3264805de4d08fb3711e54cfc34b984c7f13a38e
Shelly Leung <pontificable@yahoo.com>
2018-12-12 13:48:12 -0800

    drills 1-5 completed

---
+++ ex5.py
@@ -0,0 +1,17 @@
+myName = 'Zed A. Shaw'
+myAge = 35  # not a lie
+myHeight = 74  # inches
+myWeight = 180  # lbs
+myEyes = 'Blue'
+myTeeth = 'White'
+myHair = 'Brown'
+
+print(f"Let's talk about {myName}.")
+print(f"He's {myHeight} inches tall.")
+print(f"He's {myWeight} pounds heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {myEyes} eyes and {myHair} hair.")
+print(f"His teeth are usually {myTeeth} depending on the coffee.")
+
+total = myAge + myHeight + myWeight
+print(f"If I add {myAge}, {myHeight}, and {myWeight} I get {total}.")
